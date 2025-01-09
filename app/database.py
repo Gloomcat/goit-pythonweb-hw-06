a3b1565b5bbe7178bdef8978362c6cea383dc15d
Oleksii Aleksieiev <reneguarde@gmail.com>
2025-01-09 21:15:33 +0200

    Reorganize project structure

---
+++ app/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DATABASE_URL = "postgresql+asyncpg://postgres:1234@localhost:5432/postgres"
+# DATABASE_URL = 'postgresql+asyncpg://postgres:1234@postgres:5432/postgres'
+
+AsyncEngine = create_async_engine(DATABASE_URL, echo=True)
+AsyncSessionLocal = async_sessionmaker(bind=AsyncEngine, expire_on_commit=False)
+Base = declarative_base()
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import asyncio
 
-from goit_pythonweb_hw_06.models import async_main
+from app.database import AsyncSessionLocal
+from app.models import User
+
+
+async def async_main():
+    async with AsyncSessionLocal() as session:
+        try:
+            async with session.begin():
+                new_user = User(username="Alice", age=25)
+                session.add(new_user)
+            print("Transaction committed successfully.")
+        except Exception as e:
+            await session.rollback()
+            print(f"Transaction failed, rolled back. Error: {e}")
+
 
 if __name__ == "__main__":
     asyncio.run(async_main())
